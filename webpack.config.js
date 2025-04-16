1d2bea6108f223b4d0b3d5dc5b2846169caab732
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-04-16 20:24:45 +0200

    adding navbar

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === 'production';
-
+const isProduction = process.env.NODE_ENV === "production";
 
 const stylesHandler = MiniCssExtractPlugin.loader;
 
-
-
 const config = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, "dist"),
     },
     devServer: {
         open: true,
-        host: 'localhost',
+        host: "localhost",
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: "index.html",
+        }),
+
+        new HtmlWebpackPartialsPlugin({
+            path: path.resolve(__dirname, "src/partials/navbar.html"),
+            location: "body",
+            priority: "high",
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.resolve(__dirname, "src/partials/footer.html"),
+            location: "body",
+            priority: "low",
         }),
 
         new MiniCssExtractPlugin(),
@@ -35,24 +44,24 @@ const config = {
         rules: [
             {
                 test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                loader: "babel-loader",
             },
             {
                 test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
+                use: [stylesHandler, "css-loader"],
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'sass-loader'],
+                use: [stylesHandler, "css-loader", "sass-loader"],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                type: "asset",
             },
-            
+
             {
                 test: /\.html$/i,
-                use: ['html-loader'],
+                use: ["html-loader"],
             },
 
             // Add your rules for custom modules here
@@ -63,13 +72,11 @@ const config = {
 
 module.exports = () => {
     if (isProduction) {
-        config.mode = 'production';
-        
-        
+        config.mode = "production";
+
         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
     } else {
-        config.mode = 'development';
+        config.mode = "development";
     }
     return config;
 };
