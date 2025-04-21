08c46b9ff9eadacb89cb41f47353825afa4ce328
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-04-21 14:34:01 +0200

    make navbar links be dinamic based on page

--- src/partials/partials.js
+++ src/partials/partials.js
@@ -5,7 +5,7 @@ import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 $(document).ready(() => {
-    // navbar scrolling
+    // navbar scrolling behavior
     let scrollPos = 0;
     const $mainNav = $("#mainNav");
     const headerHeight = $mainNav.outerHeight();
@@ -14,30 +14,46 @@ $(document).ready(() => {
         const currentTop = $(document).scrollTop();
 
         if (currentTop < scrollPos) {
-            // Scrolling Up
             if (currentTop > 0 && $mainNav.hasClass("is-fixed")) {
                 $mainNav.addClass("is-visible");
             } else {
                 $mainNav.removeClass("is-visible is-fixed");
             }
         } else {
-            // Scrolling Down
             $mainNav.removeClass("is-visible");
             if (currentTop > headerHeight && !$mainNav.hasClass("is-fixed")) {
                 $mainNav.addClass("is-fixed");
             }
         }
+
         scrollPos = currentTop;
     });
 
-    // footer year copyright
+    // footer year
     const yearSpan = $("#currentYear");
     if (yearSpan) {
         yearSpan.text(new Date().getFullYear());
     }
+
+    // navbar links visibility based on page type
+    const path = window.location.pathname;
+    const isTripPage = path.includes("trip");
+    const isMainPage =
+        path.endsWith("index.html") || path === "/" || path === "";
+
+    const mainOnlyLinks = $(".main-only");
+    const tripOnlyLinks = $(".trip-only");
+
+    if (isTripPage) {
+        mainOnlyLinks.hide();
+        tripOnlyLinks.show();
+    } else {
+        mainOnlyLinks.show();
+        tripOnlyLinks.hide();
+    }
 });
 
-// fix the default icon path
+// leaflet icon fix
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -46,11 +62,14 @@ L.Icon.Default.mergeOptions({
 });
 
 const coordinates = [26.416691, 43.898653];
+const mapElement = document.getElementById("map");
 
-const map = L.map("map").setView(coordinates, 15);
+if (mapElement) {
+    const map = L.map("map").setView(coordinates, 15);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "© OpenStreetMap",
-}).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap",
+    }).addTo(map);
 
-L.marker(coordinates).addTo(map).bindPopup("سفرني للرحلات").openPopup();
+    L.marker(coordinates).addTo(map).bindPopup("سفرني للرحلات").openPopup();
+}
