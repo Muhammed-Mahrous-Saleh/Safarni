8e0dd5529af29393204887b2995d003e41848caa
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-04-22 10:39:14 +0200

    remove broken gallery auto scroll

--- src/trip.js
+++ src/trip.js
@@ -11,33 +11,4 @@ $(document).ready(function () {
     $("#scrollRight").on("click", function () {
         $gallery.animate({ scrollLeft: "+=300" }, 400);
     });
-
-    function autoScrollGallery() {
-        const scrollAmount = 260;
-        const scrollWidth = $gallery.prop("scrollWidth");
-        const visibleWidth = $gallery.width();
-        let currentScroll = $gallery.scrollLeft();
-
-        const maxScroll = scrollWidth - visibleWidth;
-        const isAtEnd = Math.abs(currentScroll - maxScroll) <= 10;
-
-        if (isAtEnd) {
-            $gallery.animate({ scrollLeft: 0 }, 600);
-        } else {
-            $gallery.animate({ scrollLeft: currentScroll + scrollAmount }, 600);
-        }
-    }
-
-    function startAutoScroll() {
-        autoScroll = setInterval(autoScrollGallery, 3000);
-    }
-
-    function stopAutoScroll() {
-        clearInterval(autoScroll);
-    }
-
-    $gallery.on("mouseenter", stopAutoScroll);
-    $gallery.on("mouseleave", startAutoScroll);
-
-    startAutoScroll();
 });
